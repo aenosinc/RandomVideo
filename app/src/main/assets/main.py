f295afe2436660be5a21fa13c5463ce60c028eab
AenosInc <emresevindi99@gmail.com>
2021-02-15 20:12:51 +0300

    algorithm changed

--- app/src/main/assets/main.py
+++ app/src/main/assets/main.py
@@ -1,11 +1,19 @@
 from youtube_search import YoutubeSearch
+from bs4 import BeautifulSoup
+import requests
 import random
-
 
 def get_random_video_id():
     while True:
-        number = random.randrange(1000, 10000)
-        word = "IMG " + str(number)
+        if random.choice([True, False]):
+            word = "IMG " + str(random.randrange(1000, 10000))
+        else:
+            try:
+                r = requests.get("https://en.wikipedia.org/wiki/Main_Page")
+                soup = BeautifulSoup(r.content,"html.parser")
+                word = random.choice(str(soup.text).split())
+            except:
+                word = "IMG " + str(random.randrange(1000, 10000))
 
         results = YoutubeSearch(word, max_results=100).to_dict()
 
